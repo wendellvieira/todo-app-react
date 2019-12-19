4276f7bfbc88f9cfdb7dbca55d6d964d179a63fe
Wendell Vieira <wendell.vieiracunha@gmail.com>
2019-12-19 09:56:39 -0300

    Configurando as Rotas (React-Router)

--- frontend/src/templates/menu.jsx
+++ frontend/src/templates/menu.jsx
@@ -11,7 +11,7 @@ export default () => (
 
             <div id="navbar" className="navbar-collapse collapse">
                 <ul className="nav navbar-nav">
-                    <li><a href="#/todo">Tarefas</a></li>
+                    <li><a href="#/todos">Tarefas</a></li>
                     <li><a href="#/about">Sobre</a></li>
                 </ul>
             </div>
