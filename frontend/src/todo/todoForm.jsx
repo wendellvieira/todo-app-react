6390235c4b258dfcc21adf0f6d1ea2bd7f012439
Wendell Vieira <wendell.vieiracunha@gmail.com>
2019-12-19 11:22:58 -0300

    Estruturando o form

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,18 @@
 import React from "react"
 
 export default ()=> (
-    <div>
-        <h1>Form</h1>
+    <div role="form" className="todoForm row">
+        <div className="col-xs-12 col-sm-9 col-md-10">
+            <input 
+                id="description" 
+                className="form-control"
+                placeholder="Adicione uma tarefa"
+            />
+        </div>
+        <div className="col-xs-12 col-sm-3 col-md-2">
+            <button className="btn btn-primary">
+                <i className="fa fa-plus"></i>
+            </button>
+        </div>
     </div>
 )
